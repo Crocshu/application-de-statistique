85c4a721bb51d99b61ac89e87a0734bdd9b90a57
Natlam-PyEnjoyer <natlam.ssl@gmail.com>
2024-12-15 16:53:54 +0100

    15_12_24_16_53

--- SAE_105_Tristan_Arnaiz_Nathan_Lamy.py
+++ SAE_105_Tristan_Arnaiz_Nathan_Lamy.py
@@ -1,28 +1,38 @@
 import pandas as pd
 import os
 import zipfile
-def ouvrir_fichier(nzip,nfile):
-    loc=os.getcwd()
-    zip=os.path.join(loc,nzip)
-    fich=os.path.join(zip,nfile)
-    with zipfile.ZipFile(zip) as myzip:
-        with myzip.open(nfile) as file:
-            ret = file.read()
-            return ret
-#print(ouvrir_fichier("medocs_mouvements.zip","mvtpdt.csv")[:1000])
-def ouvrir_fichierv2(nzip,nfile):
+
+def crea_df(fich,echan:int):
+    lire = fich.read()
+    return lire[:echan]
+
+def ouvrir_fichier(nzip,nfile,echantillon:int):
     loc=os.getcwd()
     if nzip != None :
-        print("io")
         zip=os.path.join(loc,nzip)
-        fich=os.path.join(zip,nfile)    
         with zipfile.ZipFile(zip) as myzip:
             with myzip.open(nfile) as file:
-                df = pd.read_csv(file,sep=';',header=0,index_col=0,encoding='utf-8',nrows=10)
-                return df.head(5)
+                return crea_df(fich=file,echan=echantillon).decode("latin1") # Le .decode permet de passer le type de crea_df de bytes à str car la librarie Zipfile travaille sur les données binaires
     else:
-        print("oi")
-        fich=os.path.join(loc,nfile)
-        df = pd.read_csv(fich,sep=';',header=0,index_col=0,encoding='latin1',nrows=1,chunksize=10)
-        return df
-print(ouvrir_fichierv2(nzip=None,nfile="medocs_produits.csv"))
+        with open(nfile,"r",encoding="latin1") as file:
+            return crea_df(fich=file,echan=echantillon)
+#print(ouvrir_fichier(nzip=None,nfile="medocs_produits.csv",echantillon=10000))
+#print()
+#print(ouvrir_fichier(nzip="medocs_mouvements.zip",nfile="mvtpdt.csv",echantillon=10000))
+
+
+def crea_dfv2(fich,echan:int):
+    lire = pd.read_csv(fich,sep=';',encoding='latin1',chunksize=10000,nrows=echan) #chunksize permet de diviser le fichier en plusieurs morceaux et nrows permet de prendre un nombre de lignes définies
+    return pd.concat(lire) #Concat permet de rassembler les différents chunks du fichiers, chunks fait pour ne pas surchargé la mémoire lors de l'ouverture du fichier.
+
+def ouvrir_fichierv2(nzip:str,nfile:str,echantillon:int):
+    loc=os.getcwd()
+    if nzip != None :
+        zip=os.path.join(loc,nzip)  
+        with zipfile.ZipFile(zip) as myzip:
+            with myzip.open(nfile) as file:
+                return crea_dfv2(fich=file,echan=echantillon)
+    else:
+        return crea_dfv2(fich=nfile,echan=echantillon)
+print(ouvrir_fichierv2(nzip=None,nfile="medocs_produits.csv",echantillon=10000))
+print(ouvrir_fichierv2(nzip="medocs_mouvements.zip",nfile="mvtpdt.csv",echantillon=10000))
