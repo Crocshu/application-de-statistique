17ebc538af9dfaa2a016c9f5a129e9f2d09c9085
Natlam-PyEnjoyer <natlam.ssl@gmail.com>
2024-12-16 19:55:22 +0100

    16_12_24_19_55

--- SAE_105_Tristan_Arnaiz_Nathan_Lamy.py
+++ SAE_105_Tristan_Arnaiz_Nathan_Lamy.py
@@ -3,12 +3,14 @@ import os
 import zipfile
 
 def crea_df(fich,echan:int,separ) -> dict:
-    lire = fich.read()[:echan]
-    print(type(lire),type(b'hduiaoh'))
+    lire = fich.read()
+    if echan>len(lire) : echan=len(lire)
+    lire=lire[:echan]
     if type(lire) == type(b'hduiaoh') : lire=lire.decode("latin1") # Le .decode permet de passer le type de crea_df de bytes à str car la librarie Zipfile travaille sur les données binaires
-    l_l=[w.split(separ) for w in lire.split("\n")] #Transformation de la lecture du fichier csv (str) en liste de liste
-    ini_dico={i:[] for i in l_l[0]} #Initialisation du dictionnaire
-    {ini_dico[list(ini_dico.keys())[j.index(k)]].append(k) for j in l_l for k in j} #Remplissage du dictionnaire en faisant correspondre chaque élément à sa colonne grâce à la liste de liste
+    lire=lire.replace("\r","")
+    l_l=[w.split(separ) for w in lire.split("\n")] #Transformation de la lecture du fichier csv (str) en liste de 
+    ini_dico={i:[] for i in l_l[0]} #Initialisation du dictionnaire     for x in l_l: print(l_l) for y in x: y=y.replace("\r","")
+    {ini_dico[list(ini_dico.keys())[j.index(k)]].append(k) for j in l_l[1:] for k in j} #Remplissage du dictionnaire en faisant correspondre chaque élément à sa colonne grâce à la liste de liste
     return ini_dico
 
 def ouvrir_fichier(nzip,nfile,echantillon:int,separator:str) -> dict:
@@ -21,8 +23,8 @@ def ouvrir_fichier(nzip,nfile,echantillon:int,separator:str) -> dict:
     else:
         with open(nfile,"r",encoding="latin1") as file:
             return crea_df(fich=file,echan=echantillon,separ=separator)
-print(ouvrir_fichier(nzip=None,nfile="medocs_produits.csv",echantillon=10000,separator=";"))
-print(ouvrir_fichier(nzip="medocs_mouvements.zip",nfile="mvtpdt.csv",echantillon=10000,separator=";"))
+#print(ouvrir_fichier(nzip=None,nfile="medocs_produits.csv",echantillon=10000000000,separator=";"))
+#print(ouvrir_fichier(nzip="medocs_mouvements.zip",nfile="mvtpdt.csv",echantillon=10000,separator=";"))
 
 
 def crea_dfv2(fich,echan:int,separ) -> pd.DataFrame:
@@ -39,4 +41,4 @@ def ouvrir_fichierv2(nzip:str,nfile:str,echantillon:int,separator:str)-> pd.Data
     else:
         return crea_dfv2(fich=nfile,echan=echantillon,separ=separator)
 #print(ouvrir_fichierv2(nzip=None,nfile="medocs_produits.csv",echantillon=10000,separator=";"))
-#print(ouvrir_fichierv2(nzip="medocs_mouvements.zip",nfile="mvtpdt.csv",echantillon=10000,separator=";"))
+print(ouvrir_fichierv2(nzip="medocs_mouvements.zip",nfile="mvtpdt.csv",echantillon=10000,separator=";"))
