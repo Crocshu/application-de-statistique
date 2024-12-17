9a1d99e7354c61627f017ef905ed09e309f27a10
Natlam-PyEnjoyer <natlam.ssl@gmail.com>
2024-12-17 18:07:27 +0100

    17_12_24_18_07

---
+++ modifs_crea_df.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import os
+import zipfile
+
+#Script test erreur : [x if len(x)==len(l_l[0]) and sum([1 for z in x if z==" "]) == 0 and for x in l_l]
+
+def crea_df(fich,echan:int,separ) -> dict:
+    lire = fich.read()
+    if type(lire) == type(b'hduiaoh') : lire=lire.decode("latin1") # Le .decode permet de passer le type de crea_df de bytes à str car la librarie Zipfile travaille sur les données binaires
+    lire=lire.replace("\r","")
+    l_l=[w.split(separ) for w in lire.split("\n")] #Transformation de la lecture du fichier csv (str) en liste de liste en prenant une partie du fichier suivant ce qui est demandé
+    if echan>len(l_l) : echan=len(l_l)
+    l_l=l_l[:echan]
+    l_l=[x for x in l_l if (len(x)==len(l_l[0]) and sum([1 for z in x if z=='" "']) == 0)] #Création d'une nouvelle liste sans erreurs len(x)==len(l_l[0]) and 
+    t_e=(1-round((len(l_l)/echan),3))*100 #Calcul du taux d'erreur
+    print(t_e,echan,len(l_l))
+    l_l=[[y.replace('"','') for y in x] for x in l_l]
+    ini_dico={i:[] for i in l_l[0]} #Initialisation du dictionnaire     for x in l_l: print(l_l) for y in x: y=y.replace("\r","")
+    {ini_dico[list(ini_dico.keys())[j.index(k)]].append(k) for j in l_l[1:] for k in j} #Remplissage du dictionnaire en faisant correspondre chaque élément à sa colonne grâce à la liste de liste
+    return ini_dico #[list(ini_dico.keys())[2]]
+
+def ouvrir_fichier(nzip,nfile,echantillon:int,separator:str) -> dict:
+    loc=os.getcwd()
+    if nzip != None :
+        zip=os.path.join(loc,nzip) # Joindre les chemins pour faire un script multiOS
+        with zipfile.ZipFile(zip) as myzip:
+            with myzip.open(nfile) as file:
+                return crea_df(fich=file,echan=echantillon,separ=separator)
+    else:
+        with open(nfile,"r",encoding="latin1") as file:
+            return crea_df(fich=file,echan=echantillon,separ=separator)
+#print(ouvrir_fichier(nzip=None,nfile="medocs_produits.csv",echantillon=10000000000,separator=";"))
+ouvrir_fichier(nzip="medocs_mouvements.zip",nfile="mvtpdt.csv",echantillon=10,separator=";")
+
+
+def crea_dfv2(fich,echan:int,separ) -> pd.DataFrame:
+    lire = pd.read_csv(fich,sep=separ,encoding='latin1',chunksize=10000,nrows=echan) #chunksize permet de diviser le fichier en plusieurs morceaux et nrows permet de prendre un nombre de lignes définies
+    return pd.concat(lire) #Concat permet de rassembler les différents chunks du fichiers, chunks fait pour ne pas surchargé la mémoire lors de l'ouverture du fichier.
+
+def ouvrir_fichierv2(nzip:str,nfile:str,echantillon:int,separator:str)-> pd.DataFrame:
+    loc=os.getcwd()
+    if nzip != None :
+        zip=os.path.join(loc,nzip)  
+        with zipfile.ZipFile(zip) as myzip:
+            with myzip.open(nfile) as file:
+                return crea_dfv2(fich=file,echan=echantillon,separ=separator)
+    else:
+        return crea_dfv2(fich=nfile,echan=echantillon,separ=separator)
+#print(ouvrir_fichierv2(nzip=None,nfile="medocs_produits.csv",echantillon=10000,separator=";"))
+#print(ouvrir_fichierv2(nzip="medocs_mouvements.zip",nfile="mvtpdt.csv",echantillon=1000000000,separator=";"))
--- niv1.py
+++ niv1.py
@@ -1,11 +1,18 @@
-from SAE_105_Tristan_Arnaiz_Nathan_Lamy import ouvrir_fichier
+from modifs_crea_df import ouvrir_fichier
 import matplotlib.pyplot as plt
-def graph1(df:dict,axe_x:str,axe_y:str)->plt:
+def graph1(df:dict,axe_x:str,axe_y:str,title:str)->plt:
     print(df)
-    ax,ay=df[f'"{axe_x}"'],df[f'"{axe_y}"']
-    vals_mois=[[float(z) for y,z in zip(ax,ay) if int(y[4])==x] for x in range(1,13)]# if y[4]==x
-    moy_mois=[round(sum(x)/len(x),2) for x in vals_mois]
-    print(ax,ay,"\r\n",moy_mois)
+    ax,ay=df[axe_x],df[axe_y] #Ancienne version : df[f'"{axe_x}"'],df[f'"{axe_y}"']
+    vals_mois=[[float(z.replace(",",".")) for y,z in zip(ax,ay) if y[3:5]=="{:02}".format(x)] for x in range(1,13)]# Le replace est nécessaire, sinon la conversion en float est impossible car le séparateur d'un nombre réel pour python est le point et non la virgule
+    moy_mois=[round(sum(x)/len(x),2) if len(x)!=0 else 0 for x in vals_mois ]
+    mois=["Janvier","Février","Mars","Avril","Mai","Juin","Juillet","Août","Septembre","Octobre","Novembre","Décembre"]
+    #print(ax,ay,"\r\n",vals_mois)
+    plt.bar(mois, moy_mois,color='orange')# Faire graphique en barre avec mois en valeur abscisse et moy_mois en valeur ordonnée avec une couleur orange
+    plt.xticks(rotation=90)  # Rotation de 90 degrés pour rendre les valeurs en abscisses verticales
+    plt.title(title)# Ajout du titre
+    plt.show()# Afficher le graphique
     return None
-x={'"PRCLEUNIK"': ['5926', '3716', '29207', '2034', '11675', '2386', '1855', '1856', '14522'], '"DATEMVT"': ['03/06/2020', '03/06/2020', '03/06/2020', '03/06/2020', '06/03/2020', '06/03/2020', '06/03/2020', '06/03/2020', '06/03/2020'], '"HEUREMVT"': ['"0823"', '"0823"', '"0833"', '"0833"', '"1224"', '"1224"', '"1224"', '"1224"', '"1224"'], '"SENSMVT"': ['2', '2', '2', '2', '2', '2', '2', '2', '2'], '"TYPEMVT"': ['7', '7', '7', '7', '7', '7', '7', '7', '7'], '"SERVICE"': ['"1602"', '"1602"', '"1901"', '"1901"', '"5121"', '"5121"', '"5121"', '"5121"', '"5121"'], '"MAGASIN"': ['"0433"', '"0433"', '"0433"', '"0433"', '"0433"', '"0433"', '"0433"', '"0433"', '"0433"'], '"QUANTITE"': ['20', '10', '12', '20', '56', '24', '10', '20', '20'], '"VALHT"': ['0,7', '5,197', '10,7988', '43,002', '1,0024', '0,288', '0,37', '0,598', '0,4'], '"B_URGENT"': ['1', '1', '1', '1', '0', '0', '0', '0', '0']}
-graph1(x,"DATEMVT","VALHT")
+#Valeurs test :
+#x={'PRCLEUNIK': ['5926', '3716', '29207', '2034', '11675', '2386', '1855', '1856', '14522'], 'DATEMVT': ['03/06/2020', '03/06/2020', '03/06/2020', '03/06/2020', '06/03/2020', '06/03/2020', '06/03/2020', '06/03/2020', '06/03/2020'], 'HEUREMVT': ['0823', '0823', '0833', '0833', '1224', '1224', '1224', '1224', '1224'], 'SENSMVT': ['2', '2', '2', '2', '2', '2', '2', '2', '2'], 'TYPEMVT': ['7', '7', '7', '7', '7', '7', '7', '7', '7'], 'SERVICE': ['1602', '1602', '1901', '1901', '5121', '5121', '5121', '5121', '5121'], 'MAGASIN': ['0433', '0433', '0433', '0433', '0433', '0433', '0433', '0433', '0433'], 'QUANTITE': ['20', '10', '12', '20', '56', '24', '10', '20', '20'], 'VALHT': ['0,7', '5,197', '10,7988', '43,002', '1,0024', '0,288', '0,37', '0,598', '0,4'], 'B_URGENT': ['1', '1', '1', '1', '0', '0', '0', '0', '0']}
+x=ouvrir_fichier(nzip="medocs_mouvements.zip",nfile="mvtpdt.csv",echantillon=1000000000,separator=";")
+graph1(x,"DATEMVT","VALHT",'Prix moyen des mouvements par mois')
