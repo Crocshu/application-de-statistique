7f691807e1305bb3d4da606823c376f6b282a1f4
Natlam-PyEnjoyer <natlam.ssl@gmail.com>
2025-01-13 21:22:07 +0100

    rename niv5 + solution sans la variable local='fr_FR'

--- graphiques/niv5.py
+++ graphiques/niv5.py
@@ -50,14 +50,23 @@ def graph5(df:pd.DataFrame,axe_x:str,axe_y:str,niveau:str,cherche1:str,title:str
 def graph5v2(df:pd.DataFrame):
     axe_x,axe_y,cherche1="TYPEMVT","DATEMVT",'JourSemaine'
     title='Proportion de mouvements par type de mouvement et par jour de la semaine'
-    days,colors=['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'],["red","orange","greenyellow","khaki","palegreen","green"]
+    days=jours_mapping = {
+    'Monday': 'Lundi',
+    'Tuesday': 'Mardi',
+    'Wednesday': 'Mercredi',
+    'Thursday': 'Jeudi',
+    'Friday': 'Vendredi',
+    'Saturday': 'Samedi',
+    'Sunday': 'Dimanche'
+    }
+    colors=["red","orange","greenyellow","khaki","palegreen","green"]
     dic_typemvt={1:"Livraison",3:"Facturation",4:"Retour",5:"Avoir",7:"Dispensation",8:"Régulation livraison",
     10:"Emprunt",20:"Prêt",21:"Perte",22:"Destruction",23:"Rappel", 24:"Périmé",30:"Transfert"}
     typemvt=sorted(list(df[axe_x].value_counts().index.values))
     typemvt2=[dic_typemvt[x] for x in typemvt]
     df1=pd.DataFrame(df)
     #Ajout d'une colonne avec le jour de la semaine correspondant à la datemvt
-    df1[cherche1] = pd.Categorical(pd.to_datetime(df1[axe_y],dayfirst=True).dt.day_name(locale='fr_FR'), categories=days, ordered=True)
+    df1[cherche1] = pd.Categorical(pd.to_datetime(df1[axe_y],dayfirst=True).dt.day_name().map(jours_mapping), categories=list(days.values()), ordered=True)
     df1[axe_x]=pd.Categorical(df1[axe_x].map(dic_typemvt), categories=typemvt2, ordered=True)
     data=pd.pivot_table(df1, values="QUANTITE", index=cherche1, columns=axe_x, aggfunc='count')
     total_mouvements_par_type = data.sum(axis=1)
